e7bbe51506405e29a8def927ed4eadba7633f2b3
TDanilV16 <55659112+TDanilV16@users.noreply.github.com>
2019-09-22 18:55:37 +0500

    Add files via upload

---
+++ b-sort.py
@@ -0,0 +1,12 @@
+arr = [2, 3, 5, 22, 1, 34, 0]
+
+def b_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+print(b_sort(arr))
+
